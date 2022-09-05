ae6ceb50c6766843bd0a05f80497e6150959ad25
gabriellinke <gh.linke@hotmail.com>
2022-09-04 22:34:46 -0300

    Adiciona atividade pratica 0

---
+++ Atividade Prática 0/Laboratório 0/exemplo.s
@@ -0,0 +1,49 @@
+; Exemplo.s
+; Desenvolvido para a placa EK-TM4C1294XL
+; Prof. Guilherme Peron
+; 12/03/2018
+
+; -------------------------------------------------------------------------------
+        THUMB                        ; Instruções do tipo Thumb-2
+; -------------------------------------------------------------------------------
+; Declarações EQU - Defines
+;<NOME>         EQU <VALOR>
+lista EQU 0x20000200
+primos EQU 0x20000300
+; -------------------------------------------------------------------------------
+; Área de Dados - Declarações de variáveis
+		AREA  DATA, ALIGN=2
+		; Se alguma variável for chamada em outro arquivo
+		;EXPORT  <var> [DATA,SIZE=<tam>]   ; Permite chamar a variável <var> a 
+		                                   ; partir de outro arquivo
+;<var>	SPACE <tam>                        ; Declara uma variável de nome <var>
+                                           ; de <tam> bytes a partir da primeira 
+                                           ; posição da RAM		
+
+; -------------------------------------------------------------------------------
+; Área de Código - Tudo abaixo da diretiva a seguir será armazenado na memória de 
+;                  código
+        AREA    |.text|, CODE, READONLY, ALIGN=2
+
+		; Se alguma função do arquivo for chamada em outro arquivo	
+        EXPORT Start                ; Permite chamar a função Start a partir de 
+			                        ; outro arquivo. No caso startup.s
+									
+		; Se chamar alguma função externa	
+        ;IMPORT <func>              ; Permite chamar dentro deste arquivo uma 
+									; função <func>
+		IMPORT carrega_lista
+		IMPORT encontra_e_salva_primos
+		IMPORT bubble_sort
+	
+; -------------------------------------------------------------------------------
+; Função main()
+Start  
+; Comece o código aqui <======================================================
+
+	BL carrega_lista
+	BL encontra_e_salva_primos
+	BL bubble_sort
+
+    ALIGN                           ; garante que o fim da seção está alinhada 
+    END                             ; fim do arquivo
